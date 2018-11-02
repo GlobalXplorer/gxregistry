62e2f8efc366154b78bf9a185ccee1531456d169
Val <dgm97@cornell.edu>
2018-11-02 10:56:50 -0400

    accounts endpoint displays users and their addresses

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
       router = express.Router();
 
 const APIController = require('../../controllers/api');
+const accounts = require('../../data/accounts.json');
 
 router.get('/', (req, res) => {
   res.send({
@@ -9,5 +10,9 @@ router.get('/', (req, res) => {
     status: "alive"
   });
 });
+
+router.get('/accounts', (req, res) => {
+  res.send(accounts);
+})
 
 module.exports = router;
