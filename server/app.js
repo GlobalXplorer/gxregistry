677515c3d7630731d4f4432ca3dc4af109df9b2e
Matt Fender <mhfender@gmail.com>
2018-11-02 12:12:21 -0600

    remove whitelist

--- server/app.js
+++ server/app.js
@@ -9,18 +9,18 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express();
-const whitelist = process.env.CORS.split(',');
-const corsOptions = {
-  'origin': function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+// const whitelist = process.env.CORS.split(',');
+// const corsOptions = {
+//   'origin': function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bearerToken());
 
