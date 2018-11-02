a11b2003557d08ac229b12834ac1628bc6d3a598
Val <dgm97@cornell.edu>
2018-11-02 01:46:37 -0400

    packages for bearer auth WIP

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
+      bearerToken = require('express-bearer-token'),
       routes = require('./routes');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bearerToken());
 
 routes(app);
 
