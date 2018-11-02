777c5de90cd710520f565a682cbb4056104794b0
Matt Fender <mhfender@gmail.com>
2018-11-02 12:15:17 -0600

    Remove whitelist options

--- server/app.js
+++ server/app.js
@@ -3,22 +3,7 @@ const express = require('express'),
       bearerToken = require('express-bearer-token'),
       routes = require('./routes'),
       cors = require('cors');
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load();
-}
-
 const app = express();
-// const whitelist = process.env.CORS.split(',');
-// const corsOptions = {
-//   'origin': function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
 
 app.use(cors());
 app.use(bodyParser.json());
