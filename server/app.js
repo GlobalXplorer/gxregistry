4dc2c235659f6fbfca16be47c8a7210b379f1e55
Matt Fender <mhfender@gmail.com>
2018-11-02 12:00:09 -0600

    Add cors whitelist to server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
       bearerToken = require('express-bearer-token'),
-      routes = require('./routes');
+      routes = require('./routes'),
+      cors = require('cors');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
 
 const app = express();
+const whitelist = process.env.CORS.split(',');
+const corsOptions = {
+  'origin': function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bearerToken());
 
